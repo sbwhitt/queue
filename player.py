234f8a7035cc9c2fcbc140316683c42bf9c3ff36
Samuel Whittenberger <sbw98978@gmail.com>
2020-05-19 21:34:07 -0400

    device select

--- player.py
+++ player.py
@@ -5,24 +5,20 @@ import spotipy.util as util
 from config import *
 from threading import Thread
 
-MASTER_NAME = 'rsm'
+MASTER_NAME = PLAYLIST_NAME
 
 class player(Thread):
     def __init__(self):
         Thread.__init__(self)
         scopes = 'user-read-playback-state user-modify-playback-state playlist-read-collaborative playlist-read-private playlist-modify-private'
 
-        username = 'sbw978-us'
-
-        self.laptop_id = DEVICES['laptop']
-        self.phone_id = DEVICES['phone']
-
-        token = util.prompt_for_user_token(username, scopes, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, 
+        token = util.prompt_for_user_token(USER, scopes, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, 
             redirect_uri=SPOTIPY_REDIRECT_URI)
         self.spot = spotipy.Spotify(auth=token)
 
+        self.device_id = ''
         self.master = self._get_master_playlist()
-        self.master_tracks = self.spot.user_playlist_tracks(username, playlist_id=self.master['uri'])
+        self.master_tracks = self.spot.user_playlist_tracks(USER, playlist_id=self.master['uri'])
         self.position = 0
         self.current_track = self.master_tracks['items'][self.position]
         self.start_time = 0
@@ -41,9 +37,17 @@ class player(Thread):
             offset += 50
             playlists = self.spot.current_user_playlists(limit=50, offset=offset)
         return None
+    
+    def select_device(self):
+        devices = self.spot.devices()
+        for d in devices['devices']:
+            print(d['name'])
+        
+        device_id = devices['devices'][int(input('select device: '))]['id']
+        self.device_id = device_id
 
     def start_track(self):
-        self.spot.start_playback(context_uri=self.master['uri'], offset={'position' : self.position}, device_id=self.phone_id)
+        self.spot.start_playback(context_uri=self.master['uri'], offset={'position' : self.position}, device_id=self.device_id)
         self.start_time = time.time()
 
     def run(self):
@@ -66,12 +70,12 @@ class player(Thread):
     
     def pause_track(self):
         self.paused = True
-        self.spot.pause_playback(device_id=self.phone_id)
+        self.spot.pause_playback()
         self.pause_time = time.time()
     
     def resume_track(self):
         self.paused = False
-        self.spot.start_playback(context_uri=self.master['uri'], offset={'position' : self.position}, device_id=self.phone_id)
+        self.spot.start_playback(context_uri=self.master['uri'], offset={'position' : self.position})
         time_paused = time.time() - self.pause_time
         self.start_time += time_paused
         self.spot.seek_track(self.progress)
@@ -83,14 +87,16 @@ class player(Thread):
         self.start_track()
     
     def synchronize(self):
-        self.spot.start_playback(context_uri=self.master['uri'], offset={'position' : self.position}, device_id=self.phone_id)
+        self.spot.start_playback(context_uri=self.master['uri'], offset={'position' : self.position})
         self.spot.seek_track(self.progress)
     
     def pause_music(self):
-        self.spot.pause_playback(device_id=self.phone_id)
+        self.spot.pause_playback()
 
 if __name__ == "__main__":
     p = player()
+
+    p.select_device()
     
     p.start_track()
     p.start()
